e9a7ab375e6d01879c5deab3881bd30ee0fc4466
Elliot <elliot.mcleish@gmail.com>
2021-11-26 15:43:15 +1300

    Test database connection

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-const pg = require("pg");
 const path = require("path");
 const express = require("express");
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL/* || "postgresql-colorful-38659"*/,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
 
 const PORT = process.env.PORT || 2000;
 
@@ -9,6 +15,18 @@ const app = express()
     .get('/',(req,res)=>{
         res.sendFile(path.join(__dirname,"index.html"))
     })
+    .get('/db', async (req,res)=>{
+        try{
+            const client = await pool.connect();
+            const result = await client.query('SELECT * FROM test_table');
+            const results = { 'results': (result) ? result.rows : null};
+            res.send(results);
+            client.release();
+        } catch (err){
+            console.error(err);
+            res.status(500).send(err);
+        }
+    });
 ;
 
 const server = app.listen(PORT,()=>console.log(`Server listening on port ${PORT}...`));
